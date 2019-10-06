fe9e1893a8d5944b9ccb84e1b5d5284f1bef0758
Erick 100f <erick.100f@gmail.com>
2019-10-06 08:39:02 -0500

    Initial Commit

---
+++ EXCurriculum-VIPER/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EXCurriculum-VIPER
+//
+//  Created by Erick Cienfuegos on 10/6/19.
+//  Copyright © 2019 Erick Cienfuegos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
